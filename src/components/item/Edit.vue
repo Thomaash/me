aa4d6d2fda9e55426927436e66f13cc981af1d5c
Tomáš Vyčítal <tom.vycital@gmail.com>
2018-10-28 18:44:19 +0100

    Cancel item editing by escape

--- src/components/item/Edit.vue
+++ src/components/item/Edit.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row justify-center>
-    <v-dialog v-model="dialog" persistent max-width="600px">
+    <v-dialog v-model="dialog" persistent max-width="600px" @keydown.esc="cancel">
       <v-card>
         <v-card-title>
           <span class="headline" v-text="headline"/>
