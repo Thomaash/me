798913d999cbe09a5a0a5045b022555b28e29b19
Tomáš Vyčítal <tom.vycital@gmail.com>
2018-10-31 21:43:10 +0100

    Add working link traffic controll

--- src/components/item/Link.vue
+++ src/components/item/Link.vue
@@ -6,13 +6,19 @@
           <v-text-field label="Label" required v-model="item.hostname"/>
         </v-flex>
         <v-flex xs12 sm6 md4>
-          <v-text-field label="Bandwidth" required v-model="item.bandwidth"/>
+          <v-text-field label="Bandwidth" v-model.number="item.bandwidth" min="0" suffix="MBits/s" clearable/>
         </v-flex>
         <v-flex xs12 sm6 md4>
-          <v-text-field label="Delay" required v-model="item.delay"/>
+          <v-text-field label="Delay" v-model.number="item.delay" min="0" suffix="ms" clearable/>
         </v-flex>
         <v-flex xs12 sm6 md4>
-          <v-text-field label="Loss" required v-model="item.loss"/>
+          <v-text-field label="Loss" v-model.number="item.loss" min="0" max="100" suffix="%" clearable/>
+        </v-flex>
+        <v-flex xs12 sm6 md4>
+          <v-text-field label="Max queue" v-model.number="item.maxQueueSize" min="0" suffix="packets" clearable/>
+        </v-flex>
+        <v-flex xs12 sm6 md4>
+          <v-text-field label="Jitter" v-model.number="item.jitter" min="0" suffix="ms" clearable/>
         </v-flex>
       </v-layout>
     </v-container>
