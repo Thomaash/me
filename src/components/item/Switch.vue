624ecf033cba02eb94f48be46fd1c484be6ecfca
Tomáš Vyčítal <tom.vycital@gmail.com>
2018-10-31 18:08:51 +0100

    Add switch type conf

--- src/components/item/Switch.vue
+++ src/components/item/Switch.vue
@@ -5,17 +5,29 @@
         <v-flex xs12 sm6 md4>
           <v-text-field label="Hostname" required v-model="item.hostname"/>
         </v-flex>
+        <v-flex xs12 sm6 md4>
+          <v-select label="Type" clearable :items="switchTypes" v-model="item.switchType"/>
+        </v-flex>
       </v-layout>
     </v-container>
   </v-card-text>
 </template>
 
 <script>
+const switchTypes = [
+  { value: 'IVSSwitch', text: 'IVSSwitch' },
+  { value: 'LinuxBridge', text: 'LinuxBridge' },
+  { value: 'OVSBridge', text: 'OVSBridge' },
+  { value: 'OVSSwitch', text: 'OVSSwitch' },
+  { value: 'UserSwitch', text: 'UserSwitch' }
+]
+
 export default {
   name: 'SwitchEdit',
   props: ['value'],
   data: () => ({
     dialog: false,
+    switchTypes: switchTypes,
     item: {}
   }),
   watch: {
