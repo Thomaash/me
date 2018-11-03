73df577d57718012d52126271c0984668baa76e2
Tomáš Vyčítal <tom.vycital@gmail.com>
2018-11-04 00:08:58 +0100

    Improve hints in home
    Next/prev actions.
    Place it into a card.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,21 @@
 <template>
   <v-slide-y-transition mode="out-in">
-    <v-container fluid>
-      <v-layout column align-center>
-        <blockquote v-text="hint"/>
+    <v-container grid-list-md>
+      <v-layout wrap>
+        <v-flex xs12>
+          <v-card>
+            <v-card-title primary-title>
+              <h3>Hints</h3>
+            </v-card-title>
+            <v-card-text>
+              <blockquote v-text="hint"/>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat icon @click="nextHint(-1)"><v-icon>$vuetify.icons.prev</v-icon></v-btn>
+              <v-btn flat icon @click="nextHint(1)"><v-icon>$vuetify.icons.next</v-icon></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
       </v-layout>
     </v-container>
   </v-slide-y-transition>
@@ -10,11 +23,13 @@
 
 <script>
 const hints = [
+  // Mouse
   'You can double click any item to edit it\'s settings.',
   'You can hold [ctrl] to select multiple items.',
   'You can long press a link or association to reconnect it.',
   'You can long press a switch or host to organize it\'s ports.',
 
+  // Keyboard
   'You can press [a] to fit all items into the viewport.',
   'You can press [c] to add a controller.',
   'You can press [d] to delete selected items.',
@@ -27,21 +42,27 @@ const hints = [
   'You can press [p] to add a port.',
   'You can press [s] to add a switch.',
 
+  // Other
   'First, solve the problem. Then, write the code.    — John Johnson'
 ]
 
 export default {
   name: 'Home',
   data: () => ({
-    hint: hints[hints.length - 1]
+    hintIndex: Math.floor(Math.random() * hints.length)
   }),
-  methods: {
-    randomHint () {
-      this.hint = hints[Math.floor(Math.random() * hints.length)]
+  computed: {
+    hint () {
+      return hints[this.hintIndex]
     }
   },
-  mounted () {
-    this.randomHint()
+  methods: {
+    nextHint (diff) {
+      while (diff < 0) {
+        diff += hints.length
+      }
+      this.hintIndex = (this.hintIndex + diff) % hints.length
+    }
   }
 }
 </script>
