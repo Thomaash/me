7c9105add567e946b4e8072aee9bcc2c58031e20
Tomáš Vyčítal <tom.vycital@gmail.com>
2018-11-13 23:56:03 +0100

    Test against item's type not filter type
    Wouldn't work properly without the filter.

--- tests/unit/exportImportCommon.js
+++ tests/unit/exportImportCommon.js
@@ -1,15 +1,18 @@
-function getCleanItems (items, type) {
+function getCleanItems (items, typeOnly) {
   return items.filter(node =>
-    !type ||
-    node.type === type
+    !typeOnly ||
+    node.type === typeOnly
   ).map(orig => {
+    const type = orig.type
+    const isEdge = type === 'link' || type === 'association'
+
     const clean = {}
     Object.keys(orig).forEach(key => {
       if (type === 'port' && key === 'ips') {
         clean[key] = orig[key].sort()
       } else if (
-        (type === 'link' && !/^(id|hostname|from|to)$/.test(key)) ||
-        (type !== 'link' && !/^(id|x|y)$/.test(key))
+        (isEdge && !/^(id|hostname|from|to)$/.test(key)) ||
+        (!isEdge && !/^(id|x|y)$/.test(key))
       ) {
         clean[key] = orig[key]
       }
