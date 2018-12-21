ef3bdc3d0167d9025e7b11fd6b59b39a3a4ac274
Tomáš Vyčítal <tom.vycital@gmail.com>
2018-12-22 00:28:55 +0100

    Improve persiting
    - Prompt the user before leaving when there are usaved changes.
    - Don't trigger saving by unpersisted changes.
    - Delay each saving 2 seconds.
    - Abort previous saving if a new change happend within the 2 seconds.

--- src/store/persist.js
+++ src/store/persist.js
@@ -10,23 +10,53 @@ localForage.config({
   storeName: 'vuex-me'
 })
 
+let saveTimeout = null
+let saving = 0
+
+// Don't let the user leave the page as long as one or more changes are being saved.
+window.addEventListener('beforeunload', e => {
+  if (saving) {
+    e.preventDefault()
+    e.returnValue = ''
+  }
+})
+
 export default new VuexPersist({
   storage: localForage,
   asyncStorage: true,
   strictMode: false,
+  filter: ({ type }) => type.startsWith('topology/'),
   reducer: state => ({ topology: state.topology }),
-  async saveState (key, state, storage) {
-    return storage.setItem(key, {
-      topology: {
-        data: exporter.exportData(state.topology.data),
-        past: state.topology.past,
-        future: state.topology.future
-      }
-    })
+  saveState (key, state, storage) {
+    if (saveTimeout != null) {
+      window.clearTimeout(saveTimeout)
+    } else {
+      ++saving
+    }
+
+    saveTimeout = window.setTimeout(() => {
+      saveTimeout = null
+      ;(async () => {
+        await storage.setItem(key, {
+          topology: {
+            data: exporter.exportData(state.topology.data),
+            past: state.topology.past,
+            future: state.topology.future
+          }
+        })
+        --saving
+      })()
+    }, 2000)
   },
   async restoreState (key, storage) {
     const state = await storage.getItem(key)
-    if (state && state.topology && state.topology.data && state.topology.data.items && Array.isArray(state.topology.data.items)) {
+    if (
+      state &&
+      state.topology &&
+      state.topology.data &&
+      state.topology.data.items &&
+      Array.isArray(state.topology.data.items)
+    ) {
       return {
         loading: false,
         topology: {
