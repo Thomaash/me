48baf7df25b4d0c873eca460ec0896b931266cda
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-01-07 20:07:29 +0100

    Tweak eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,12 +21,14 @@ module.exports = {
   rules: {
     // reported by vue-cli-service lint
     'vue/html-closing-bracket-spacing': 'error',
-    'vue/max-attributes-per-line': 'off',
     // [] spacing
     'array-bracket-spacing': ['error', 'never'],
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // off
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
   }
 }
