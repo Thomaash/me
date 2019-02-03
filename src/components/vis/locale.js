a30c1919f9b197def4021af2aea7c84e36c21138
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-02-03 14:26:57 +0100

    Fix item sorting

--- src/components/vis/locale.js
+++ src/components/vis/locale.js
@@ -1,7 +1,7 @@
 const collator = new Intl.Collator(undefined, { numeric: true })
 const compare = collator.compare
 const compareNodes = (a, b) => compare(a.label, b.label)
-const compareItems = (a, b) => compare(a.label, b.label)
+const compareItems = (a, b) => compare(a.hostname, b.hostname)
 
 export {
   collator,
