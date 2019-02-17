fe506ccf304f1864af126ecaeddf8bdcd6e3ce12
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-02-17 19:06:41 +0100

    Simplify the layout

--- src/components/Export.vue
+++ src/components/Export.vue
@@ -1,30 +1,22 @@
 <template>
   <v-container grid-list-md>
     <v-layout wrap>
-      <v-flex xs12>
+      <v-flex xs12 pb-5>
         <h3 class="headline">Import</h3>
-      </v-flex>
 
-      <v-flex xs12>
         <Imports @log="l => log = l" />
       </v-flex>
 
-      <v-flex xs12 pt-5>
+      <v-flex xs12 pb-5>
         <h3 class="headline">Export</h3>
-      </v-flex>
 
-      <v-flex xs12>
         <Exports @log="l => log = l" />
       </v-flex>
 
       <v-slide-y-transition mode="out-in">
-        <v-flex xs12 pt-5 v-if="log.length">
+        <v-flex xs12 pb-5 v-if="log.length">
           <h3 class="headline">Log</h3>
-        </v-flex>
-      </v-slide-y-transition>
 
-      <v-slide-y-transition mode="out-in">
-        <v-flex xs12 v-if="log.length">
           <Log :log="log" />
         </v-flex>
       </v-slide-y-transition>
