d2823347aa71135336635012ef5224d5cf75826c
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-02-17 18:01:16 +0100

    Test adding and also deleting of more items
    Port, host, switch and controller.

--- tests/e2e/specs/canvas.js
+++ tests/e2e/specs/canvas.js
@@ -5,40 +5,60 @@ describe('Canvas', () => {
     cy.meClickMenu('Canvas')
   })
 
-  it('Add a new host', () => {
-    cy.get('.v-speed-dial > button')
-      .click()
-    cy.get('.v-speed-dial__list > button:nth-child(3)')
-      .click()
+  ;[
+    { type: 'port', hostname: 'eth0', heading: 'Port', itemsToDelete: 1 },
+    { type: 'host', hostname: 'h1', heading: 'Host', itemsToDelete: 3 },
+    { type: 'switch', hostname: 's1', heading: 'Switch', itemsToDelete: 7 },
+    { type: 'controller', hostname: 'c1', heading: 'Controller', itemsToDelete: 1 }
+  ].forEach(({ type, hostname, heading, itemsToDelete }) => {
+    describe(type, () => {
+      it('Enter add mode', () => {
+        cy.meVisFabClick(type)
 
-    cy.get('.vis-root')
-      .trigger('mousemove', { button: 0, clientX: 50, clientY: 50 })
-      .trigger('mousemove', { button: 0, clientX: 150, clientY: 50 })
-    cy.get('.mouse-tag')
+        cy.get('.vis-root')
+          .trigger('mousemove', { button: 0, clientX: 50, clientY: 50 })
+          .trigger('mousemove', { button: 0, clientX: 150, clientY: 50 })
+        cy.get('.mouse-tag')
+      })
 
-    cy.get('canvas')
-      .meVisClick({ x: 150, y: 150 })
-    const header = cy.contains('h3', 'Host')
-    cy.get('input')
-      .should('have.value', 'h1')
+      it('Place the item', () => {
+        cy.get('canvas')
+          .meVisClick({ x: 150, y: 150 })
+        const header = cy.contains('h3', heading)
+        cy.get('input')
+          .should('have.value', hostname)
 
-    cy.focused()
-      .type('{enter}')
-    header
-      .should('not.exist')
-  })
+        cy.focused()
+          .type('{enter}')
+        header
+          .should('not.exist')
+      })
 
-  it('Test the new host\'s existence', () => {
-    cy.get('canvas')
-      .meVisClick({ x: 150, y: 150 })
-      .meVisClick({ x: 150, y: 150 })
-    const header = cy.contains('h3', 'Host')
-    cy.get('input')
-      .should('have.value', 'h1')
+      it('Test the items\'s existence', () => {
+        // Cypress seems to be too fast sometimes
+        cy.wait(1500)
 
-    cy.focused()
-      .type('{enter}')
-    header
-      .should('not.exist')
+        cy.get('canvas')
+          .meVisClick({ x: 150, y: 150, repeat: 2 })
+        const header = cy.contains('h3', heading)
+        cy.get('input')
+          .should('have.value', hostname)
+
+        cy.focused()
+          .type('{enter}')
+        header
+          .should('not.exist')
+      })
+
+      it(`Delete the ${type}`, () => {
+        cy.get('canvas')
+          .meVisClick({ x: 150, y: 150 })
+
+        cy.meVisFabClick('delete')
+
+        cy.get('.v-snack__content')
+          .contains(`${itemsToDelete} item${itemsToDelete === 1 ? '' : 's'} deleted`)
+      })
+    })
   })
 })
--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -27,12 +27,36 @@
 Cypress.Commands.add(
   'meVisClick',
   { prevSubject: 'element' },
-  (subject, { button = 0, x = 0, y = 0 }) => {
-    return cy.wrap(subject)
-      .trigger('pointerdown', { button, clientX: x, clientY: y })
-      .trigger('pointerup', { button, clientX: x, clientY: y })
+  (subject, { button = 0, x = 0, y = 0, repeat = 1 }) => {
+    for (let i = 0; i < repeat; ++i) {
+      cy.wrap(subject)
+        .trigger('pointerdown', { button, clientX: x, clientY: y })
+        .trigger('pointerup', { button, clientX: x, clientY: y })
+    }
   }
 )
+
+;(() => {
+  const buttons = {
+    'edge': 1,
+    'port': 2,
+    'host': 3,
+    'switch': 4,
+    'controller': 5,
+    'label': 6,
+    'delete': 7
+  }
+  Cypress.Commands.add(
+    'meVisFabClick',
+    { prevSubject: false },
+    (button) => {
+      cy.get('.v-speed-dial > button')
+        .click()
+      cy.get(`.v-speed-dial__list > button:nth-child(${buttons[button]})`)
+        .click()
+    }
+  )
+})()
 
 Cypress.Commands.add(
   'meImportEmpty',
