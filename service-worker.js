c3c0db63c06a83af01b79dd83166aa3b5dfa9529
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-06-11 13:42:44 +0200

    Update GitHub pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.243e5e1cae8e9259761ac2678e8ece52.js"
+  "precache-manifest.a88763f6d16d174f3f427e0274f7f7ae.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mininet-editor"});
