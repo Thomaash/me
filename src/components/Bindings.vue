d66a43d6e390497f526472ffaf6c79c63eafb25e
Tomáš Vyčítal <tom.vycital@gmail.com>
2020-05-26 12:26:37 +0200

    fix(bindings): broken icons and titles

--- src/components/Bindings.vue
+++ src/components/Bindings.vue
@@ -2,19 +2,20 @@
   <section>
     <h3 class="headline">Bindings</h3>
 
-    <v-data-table :headers="headers" :items="items" :items-per-page="items.length" :sort-by="[]" hide-default-footer class="elevation-1">
-      <template slot="items" slot-scope="props">
-        <td class="bindings">
+    <v-data-table :headers="headers" :items="items" item-key="combination" :items-per-page="items.length" :sort-by="[]" hide-default-footer class="elevation-1">
+      <template #item.combination="{ item }">
+        <div class="with-title combination" :title="getTitleFor(item.combination)">
           <span
             :is="c.tag"
-            v-for="(c, i) in parse(props.item.combination)"
+            v-for="(c, i) in parse(item.combination)"
             :key="i"
-            :title="c.title"
-            class="with-title monospace"
+            class="monospace"
             v-text="c.text"
           />
-        </td>
-        <td v-text="props.item.description" />
+        </div>
+      </template>
+      <template #item.description="{ item }">
+        {{ item.description }}
       </template>
     </v-data-table>
   </section>
@@ -33,7 +34,7 @@ export default {
       { combination: 'LMB - :port', description: 'Place a new port (connects to a nearby switch or host).' },
       { combination: 'LMB @ :item', description: 'Select the node or edge.' },
       { combination: 'LMB2 @ :item', description: 'Edit the item.' },
-      { combination: 'LMBd :edge', description: 'Connect two nodes with a link or an association.' },
+      { combination: 'LMBd - :edge', description: 'Connect two nodes with a link or an association.' },
       { combination: 'LMBd @ :node', description: 'Move the node.' },
       { combination: 'LMBd', description: 'Move the viewport.' },
       { combination: 'LMBlp @ :edge', description: 'Reconnect a link or an association.' },
@@ -65,27 +66,27 @@ export default {
       { combination: 'esc', description: 'Stop editing edges or adding items.' }
     ],
     special: {
-      '@': { tag: 'span', text: ' @ ', title: 'On' },
-      '+': { tag: 'span', text: ' + ', title: 'Both together' },
-      '-': { tag: 'span', text: ' ', title: 'With' },
+      '@': { tag: 'span', text: ' @ ', title: 'on' },
+      '+': { tag: 'span', text: ' + ', title: 'and' },
+      '-': { tag: 'span', text: ' ', title: 'with' },
 
-      LMB: { tag: 'kbd', text: 'LMB', title: 'Left mouse button click' },
-      LMB2: { tag: 'kbd', text: '2·LMB', title: 'Double left mouse button click' },
-      LMBd: { tag: 'kbd', text: 'LMB →', title: 'Drag with left mouse button pressed' },
-      LMBlp: { tag: 'kbd', text: '   LMB   ', title: 'Long press left mouse button' },
-      RMBd: { tag: 'kbd', text: 'RMB →', title: 'Drag with right mouse button pressed' },
+      LMB: { tag: 'kbd', text: 'LMB', title: 'left mouse button click' },
+      LMB2: { tag: 'kbd', text: '2·LMB', title: 'double left mouse button click' },
+      LMBd: { tag: 'kbd', text: 'LMB →', title: 'drag with left mouse button pressed' },
+      LMBlp: { tag: 'kbd', text: '   LMB   ', title: 'long press left mouse button' },
+      RMBd: { tag: 'kbd', text: 'RMB →', title: 'drag with right mouse button pressed' },
 
-      ctrl: { tag: 'kbd', text: 'Ctrl', title: 'Control' },
-      shift: { tag: 'kbd', text: 'Shift', title: 'Shift' },
+      ctrl: { tag: 'kbd', text: 'CTRL', title: 'hold control' },
+      shift: { tag: 'kbd', text: 'SHIFT', title: 'hold shift' },
 
-      del: { tag: 'kbd', text: 'Del', title: 'Delete' },
-      esc: { tag: 'kbd', text: 'Esc', title: 'Escape' },
+      del: { tag: 'kbd', text: 'DEL', title: 'press delete' },
+      esc: { tag: 'kbd', text: 'ESC', title: 'press escape' },
 
-      ':edge': { tag: 'v-icon', text: '$vuetify.icons.net-edge', title: 'Link or association' },
-      ':item': { tag: 'v-icon', text: '$vuetify.icons.net-host', title: 'Port, host, switch, controller, label, link or association' },
-      ':node': { tag: 'v-icon', text: '$vuetify.icons.net-host', title: 'Port, host, switch, controller or label' },
-      ':swho': { tag: 'v-icon', text: '$vuetify.icons.net-host', title: 'Switch or host' },
-      ':port': { tag: 'v-icon', text: '$vuetify.icons.net-port', title: 'Port' }
+      ':edge': { tag: 'v-icon', text: '$vuetify.icons.net-edge', title: 'link or association' },
+      ':item': { tag: 'v-icon', text: '$vuetify.icons.net-host', title: 'port, host, switch, controller, label, link or association' },
+      ':node': { tag: 'v-icon', text: '$vuetify.icons.net-host', title: 'port, host, switch, controller or label' },
+      ':swho': { tag: 'v-icon', text: '$vuetify.icons.net-host', title: 'switch or host' },
+      ':port': { tag: 'v-icon', text: '$vuetify.icons.net-port', title: 'port' }
     },
     nodeIcons: ['port', 'host', 'switch', 'controller', 'dummy'],
     itemIcons: ['edge', 'port', 'host', 'switch', 'controller', 'dummy'],
@@ -108,11 +109,14 @@ export default {
           return this.special[val]
         } else if (this.simpleKeyRE.test(val)) {
           const text = val.toUpperCase()
-          return { tag: 'kbd', text: text, title: text }
+          return { tag: 'kbd', text: text, title: 'press ' + text }
         } else {
           return { tag: 'span', text: ' ' + val + ' ', title: val }
         }
       })
+    },
+    getTitleFor (str) {
+      return this.parse(str).map(c => c.title).join(' ')
     },
     update () {
       ++this.iconsIndex
@@ -125,10 +129,23 @@ export default {
 </script>
 
 <style scoped>
-.bindings > * {
-  vertical-align: middle;
-}
 .with-title {
   cursor: help;
+}
+.combination {
+  white-space: nowrap;
+}
+kbd {
+  background-color: #eee;
+  border-radius: 3px;
+  border: 1px solid #b4b4b4;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, .2), 0 2px 0 0 rgba(255, 255, 255, .7) inset;
+  color: #333;
+  display: inline-block;
+  font-size: .85em;
+  font-weight: 700;
+  line-height: 1;
+  padding: 2px 4px;
+  white-space: nowrap;
 }
 </style>
