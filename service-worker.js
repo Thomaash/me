a37c2fe39dbe8512216b8170d8f36187d3b3888a
Tomáš Vyčítal <tomina@vycital.eu>
2021-11-18 04:04:30 +0000

    chore: update GitHub Pages [ci skip]

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c63dbd19df091e8efdd7b8cc5e55a281.js"
+  "precache-manifest.03a0c619dab8e7a31aa1e253424d4d3c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mininet-editor"});
