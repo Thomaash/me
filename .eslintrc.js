3731a1f8279eff35e144ac2281eafe4c52696457
Tomina <tomina@vycital.eu>
2022-01-26 20:10:52 +0100

    lint: switch to Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,32 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    parser: 'babel-eslint',
-    sourceType: 'module'
+    parser: "@babel/eslint-parser",
+    sourceType: "module",
   },
-  env: {
-    browser: true
+  globals: {
+    module: "writable",
+    process: "readonly",
   },
-  extends: [
-    'plugin:vue/recommended'
-  ],
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
+  extends: ["plugin:vue/recommended", "eslint:recommended", "@vue/prettier"],
+  plugins: ["vue", "prettier"],
   rules: {
-    // reported by vue-cli-service lint
-    'vue/html-closing-bracket-spacing': 'error',
-    // [] spacing
-    'array-bracket-spacing': ['error', 'never'],
-    // allow async-await
-    'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    // off
-    'vue/max-attributes-per-line': 'off',
-    'vue/singleline-html-element-content-newline': 'off'
-  }
-}
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  overrides: [
+    {
+      files: ["./src/**", "./tests/**"],
+      env: {
+        browser: true,
+      },
+    },
+    {
+      files: ["./*.*", "./.*.*"],
+      env: {
+        node: true,
+      },
+    },
+  ],
+};
