e3c86d8d6c6b6b288cbd5b2fda258d1bcb08c01e
Tomina <tomina@vycital.eu>
2025-05-10 15:53:46 +0200

    refactor(service-worker): log an error when registration fails
    Showing a toast to the user would maybe be better? But there is nothing
    the user could do anyway.

--- src/service-worker-init.js
+++ src/service-worker-init.js
@@ -2,10 +2,15 @@ import { store } from "./store";
 
 export function initServiceWorker() {
   if (!("Cypress" in globalThis) && "serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./service-worker.js").then((reg) => {
-      reg?.addEventListener("updatefound", function () {
-        store.commit("setUpdateAvailable");
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((reg) => {
+        reg?.addEventListener("updatefound", function () {
+          store.commit("setUpdateAvailable");
+        });
+      })
+      .catch((error) => {
+        console.error(error, "Failed to register service worker");
       });
-    });
   }
 }
