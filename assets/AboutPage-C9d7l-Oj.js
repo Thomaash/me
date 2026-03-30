bd1046185aff2075d55ccd8646265a7f341a5393
Reimagined Palm Tree <reimagined-palm-tree@vycital.eu>
2026-03-30 18:44:48 +0000

    chore: update GitHub Pages [ci skip]

---
+++ assets/AboutPage-C9d7l-Oj.js
@@ -0,0 +1,16 @@
+import{u as I}from"./useTopologyStore-DIXibazd.js";import{al as s,am as i,F as n,a8 as u,aa as p,aT as l,E as e,an as a,V as O,aZ as d,a4 as N,ao as R,e as S,ap as _}from"./js/app-DABgpy4a.js";import{A as C,_ as f}from"./AppLogo-Bpw88Kak.js";import{b,V as L,a as T}from"./VRow-04htHKky.js";import"./VCard-BN561VMh.js";const H={key:0},h=Object.assign({name:"BuildInfo"},{__name:"BuildInfo",setup(E){const{isUpdateAvailable:r}=I(),m="2026-03-30T18:37:12.771Z",o="e559c42b3c023be22b55e85a20004a140e6ab288",A="2026-03-30T18:36:03.000Z";return(c,t)=>(s(),i("section",null,[t[4]||(t[4]=n("h3",{class:"text-h6"},"Build",-1)),n("p",null,[u(" Build date: "+p(l(m))+" ",1),t[1]||(t[1]=n("br",null,null,-1)),u(" Commit date: "+p(l(A))+" ",1),t[2]||(t[2]=n("br",null,null,-1)),u(" Commit hash: "+p(l(o))+" ",1),t[3]||(t[3]=n("br",null,null,-1)),e(O,{class:"ma-2",variant:"outlined",color:"primary",target:"_blank",href:"https://github.com/Thomaash/me/commit/"+l(o)},{default:a(()=>[...t[0]||(t[0]=[u(" Open on GitHub ",-1)])]),_:1},8,["href"])]),l(r)?(s(),i("p",H," A new version is available and will be automatically installed when you close all open tabs. ")):d("",!0)]))}}),D=`ISC License
+
+Copyright (c) 2018-2025, Tomáš Vyčítal
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+`,F=["textContent"],g=Object.assign({name:"AppLicense"},{__name:"AppLicense",setup(E){const r=S(()=>D.split(/\n\n/g));return(m,o)=>(s(),i("section",null,[o[0]||(o[0]=n("h3",{class:"text-h6"},"License",-1)),(s(!0),i(N,null,R(r.value,(A,c)=>(s(),i("p",{key:"license_p_"+c,textContent:p(A)},null,8,F))),128))]))}}),w={__name:"AboutPage",setup(E){return(r,m)=>(s(),_(b,{"grid-list-md":""},{default:a(()=>[e(L,{wrap:""},{default:a(()=>[e(C),e(T,{cols:"12"},{default:a(()=>[e(f,{full:""})]),_:1}),e(T,{cols:"12"},{default:a(()=>[e(g)]),_:1}),e(T,{cols:"12"},{default:a(()=>[e(h)]),_:1})]),_:1})]),_:1}))}};export{w as default};
