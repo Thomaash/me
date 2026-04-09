ba4c3570e083e7b130da2b8b39c9d971afaa5c82
Reimagined Palm Tree <reimagined-palm-tree@vycital.eu>
2026-04-09 12:41:14 +0000

    chore: update GitHub Pages [ci skip]

---
+++ assets/AboutPage-DE6qjdTC.js
@@ -0,0 +1,16 @@
+import{u as I}from"./useTopologyStore-x_vlFB6L.js";import{al as s,am as i,F as n,a8 as u,aa as p,aU as l,E as e,an as a,V as d,a_ as O,a4 as N,ao as R,e as S,ap as _}from"./js/app-CB_CxMGk.js";import{A as f,_ as C}from"./AppLogo-DjezYGno.js";import{b as L,V as b,a as A}from"./VRow-D-cuh7A7.js";import"./VCard-Cl0Sotyq.js";const H={key:0},h=Object.assign({name:"BuildInfo"},{__name:"BuildInfo",setup(T){const{isUpdateAvailable:r}=I(),c="2026-04-09T12:33:57.618Z",o="c3ecf47d3881a6a467c8b2a159f1bcaecc9c9c01",m="2026-04-09T12:32:36.000Z";return(E,t)=>(s(),i("section",null,[t[4]||(t[4]=n("h3",{class:"text-h6"},"Build",-1)),n("p",null,[u(" Build date: "+p(l(c))+" ",1),t[1]||(t[1]=n("br",null,null,-1)),u(" Commit date: "+p(l(m))+" ",1),t[2]||(t[2]=n("br",null,null,-1)),u(" Commit hash: "+p(l(o))+" ",1),t[3]||(t[3]=n("br",null,null,-1)),e(d,{class:"ma-2",variant:"outlined",color:"primary",target:"_blank",href:"https://github.com/Thomaash/me/commit/"+l(o)},{default:a(()=>[...t[0]||(t[0]=[u(" Open on GitHub ",-1)])]),_:1},8,["href"])]),l(r)?(s(),i("p",H," A new version is available and will be automatically installed when you close all open tabs. ")):O("",!0)]))}}),D=`ISC License
+
+Copyright (c) 2018-2025, Tomáš Vyčítal
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+`,F=["textContent"],g=Object.assign({name:"AppLicense"},{__name:"AppLicense",setup(T){const r=S(()=>D.split(/\n\n/g));return(c,o)=>(s(),i("section",null,[o[0]||(o[0]=n("h3",{class:"text-h6"},"License",-1)),(s(!0),i(N,null,R(r.value,(m,E)=>(s(),i("p",{key:"license_p_"+E,textContent:p(m)},null,8,F))),128))]))}}),w={__name:"AboutPage",setup(T){return(r,c)=>(s(),_(L,{"grid-list-md":""},{default:a(()=>[e(b,{wrap:""},{default:a(()=>[e(f),e(A,{cols:"12"},{default:a(()=>[e(C,{full:""})]),_:1}),e(A,{cols:"12"},{default:a(()=>[e(g)]),_:1}),e(A,{cols:"12"},{default:a(()=>[e(h)]),_:1})]),_:1})]),_:1}))}};export{w as default};
