18a8636e39056127eab11d329661342e74525455
Reimagined Palm Tree <reimagined-palm-tree@vycital.eu>
2026-04-22 16:16:00 +0000

    chore: update GitHub Pages [ci skip]

---
+++ assets/AboutPage-CYTk0nLX.js
@@ -0,0 +1,16 @@
+import{An as e,Cn as t,Dn as n,En as r,Jn as i,Kn as a,Mn as o,On as s,jn as c,kn as l,nr as u,wr as d,yr as f}from"./position-BECyr_Ey.js";import{t as p}from"./VBtn-CJVtFScG.js";import{n as m,r as h,t as g}from"./VRow-DlomNP3r.js";import{t as _}from"./useTopologyStore-BRGxGfPB.js";import{n as v,t as y}from"./AppLogo-Cu6T2S0y.js";var b={key:0},x=Object.assign({name:`BuildInfo`},{__name:`BuildInfo`,setup(t){let{isUpdateAvailable:r}=_(),i=`335507ae5ce04094b16263203f550594479ca184`;return(t,s)=>(a(),e(`section`,null,[s[4]||=n(`h3`,{class:`text-h6`},`Build`,-1),n(`p`,null,[c(` Build date: `+d(f(`2026-04-22T16:05:24.577Z`))+` `,1),s[1]||=n(`br`,null,null,-1),c(` Commit date: `+d(f(`2026-04-22T16:04:34.000Z`))+` `,1),s[2]||=n(`br`,null,null,-1),c(` Commit hash: `+d(f(i))+` `,1),s[3]||=n(`br`,null,null,-1),o(p,{class:`ma-2`,variant:`outlined`,color:`primary`,target:`_blank`,href:`https://github.com/Thomaash/me/commit/`+f(i)},{default:u(()=>[...s[0]||=[c(` Open on GitHub `,-1)]]),_:1},8,[`href`])]),f(r)?(a(),e(`p`,b,` A new version is available and will be automatically installed when you close all open tabs. `)):l(``,!0)]))}}),S=`ISC License
+
+Copyright (c) 2018-2025, Tomáš Vyčítal
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+`,C=[`textContent`],w=Object.assign({name:`AppLicense`},{__name:`AppLicense`,setup(o){let s=r(()=>S.split(/\n\n/g));return(r,o)=>(a(),e(`section`,null,[o[0]||=n(`h3`,{class:`text-h6`},`License`,-1),(a(!0),e(t,null,i(s.value,(t,n)=>(a(),e(`p`,{key:`license_p_`+n,textContent:d(t)},null,8,C))),128))]))}}),T={__name:`AboutPage`,setup(e){return(e,t)=>(a(),s(h,{"grid-list-md":``},{default:u(()=>[o(g,{wrap:``},{default:u(()=>[o(y),o(m,{cols:`12`},{default:u(()=>[o(v,{full:``})]),_:1}),o(m,{cols:`12`},{default:u(()=>[o(w)]),_:1}),o(m,{cols:`12`},{default:u(()=>[o(x)]),_:1})]),_:1})]),_:1}))}};export{T as default};
