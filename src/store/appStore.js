2862d7538712764836c78103284e217701b4f6af
Tomina <tomina@vycital.eu>
2026-05-06 15:28:42 +0000

    refactor(store): convert appStore to setup syntax

--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,44 +1,67 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useAppStore = defineStore("app", {
-  state: () => ({
-    loading: true,
-    working: false,
-    isUpdateAvailable: false,
-    alert: { show: false },
-    saveState: "idle", // "idle" | "pending" | "saving" | "error"
-  }),
-  actions: {
-    loaded() {
-      this.loading = false;
-    },
-    setWorking({ working, curr, max }) {
-      if (!isNaN(curr) && !isNaN(max)) {
-        this.working = { curr, max };
-      } else {
-        this.working = !!working;
-      }
-    },
-    setAlert({ type, text }) {
-      this.alert = { show: true, type, text };
-    },
-    clearAlert() {
-      this.alert.show = false;
-    },
-    setUpdateAvailable() {
-      this.isUpdateAvailable = true;
-    },
-    markPending() {
-      this.saveState = "pending";
-    },
-    markSaving() {
-      this.saveState = "saving";
-    },
-    markSaved() {
-      this.saveState = "idle";
-    },
-    markSaveError(_err) {
-      this.saveState = "error";
-    },
-  },
+export const useAppStore = defineStore("app", () => {
+  const loading = ref(true);
+  const working = ref(false);
+  const isUpdateAvailable = ref(false);
+  const alert = ref({ show: false });
+  const saveState = ref("idle"); // "idle" | "pending" | "saving" | "error"
+
+  function loaded() {
+    loading.value = false;
+  }
+
+  function setWorking({ working: w, curr, max }) {
+    if (!isNaN(curr) && !isNaN(max)) {
+      working.value = { curr, max };
+    } else {
+      working.value = !!w;
+    }
+  }
+
+  function setAlert({ type, text }) {
+    alert.value = { show: true, type, text };
+  }
+
+  function clearAlert() {
+    alert.value.show = false;
+  }
+
+  function setUpdateAvailable() {
+    isUpdateAvailable.value = true;
+  }
+
+  function markPending() {
+    saveState.value = "pending";
+  }
+
+  function markSaving() {
+    saveState.value = "saving";
+  }
+
+  function markSaved() {
+    saveState.value = "idle";
+  }
+
+  function markSaveError(_err) {
+    saveState.value = "error";
+  }
+
+  return {
+    loading,
+    working,
+    isUpdateAvailable,
+    alert,
+    saveState,
+    loaded,
+    setWorking,
+    setAlert,
+    clearAlert,
+    setUpdateAvailable,
+    markPending,
+    markSaving,
+    markSaved,
+    markSaveError,
+  };
 });
