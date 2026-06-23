49e36ed8e885c18c220410bf0763ae7b10063232
Reimagined Palm Tree <reimagined-palm-tree@vycital.eu>
2026-06-23 22:25:56 +0000

    chore: update GitHub Pages [ci skip]

---
+++ assets/AboutPage-DD5V5CQk.js
@@ -0,0 +1,16 @@
+import{An as e,Dn as t,Jn as n,Mn as r,Nn as i,On as a,Or as o,Sr as s,Xn as c,ar as l,jn as u,kn as d,wn as f}from"./position-b9j0lIdm.js";import{n as p,r as m,t as h}from"./VRow-82Vw2siZ.js";import{g,m as _}from"./js/app-DbUcDkVd.js";import{n as v,t as y}from"./AppLogo-CQId8bMu.js";var b={key:0},x=Object.assign({name:`BuildInfo`},{__name:`BuildInfo`,setup(t){let c=g(),d=`89bd5a9249796fa3efdd753f56c7c91a5a2662b2`;return(t,f)=>(n(),u(`section`,null,[f[4]||=a(`h3`,{class:`text-h6`},`Build`,-1),a(`p`,null,[r(` Build date: `+o(s(`2026-06-23T22:16:25.071Z`))+` `,1),f[1]||=a(`br`,null,null,-1),r(` Commit date: `+o(s(`2026-06-23T22:15:12.000Z`))+` `,1),f[2]||=a(`br`,null,null,-1),r(` Commit hash: `+o(s(d))+` `,1),f[3]||=a(`br`,null,null,-1),i(_,{class:`ma-2`,variant:`outlined`,color:`primary`,target:`_blank`,href:`https://github.com/Thomaash/me/commit/`+s(d)},{default:l(()=>[...f[0]||=[r(` Open on GitHub `,-1)]]),_:1},8,[`href`])]),s(c).isUpdateAvailable?(n(),u(`p`,b,` A new version is available and will be automatically installed when you close all open tabs. `)):e(``,!0)]))}}),S=`ISC License
+
+Copyright (c) 2018-2026, Tomáš Vyčítal
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+`,C=[`textContent`],w=Object.assign({name:`AppLicense`},{__name:`AppLicense`,setup(e){let r=t(()=>S.split(/\n\n/g));return(e,t)=>(n(),u(`section`,null,[t[0]||=a(`h3`,{class:`text-h6`},`License`,-1),(n(!0),u(f,null,c(r.value,(e,t)=>(n(),u(`p`,{key:`license_p_`+t,textContent:o(e)},null,8,C))),128))]))}}),T={__name:`AboutPage`,setup(e){return(e,t)=>(n(),d(m,{"grid-list-md":``},{default:l(()=>[i(h,{wrap:``},{default:l(()=>[i(y),i(p,{cols:`12`},{default:l(()=>[i(v,{full:``})]),_:1}),i(p,{cols:`12`},{default:l(()=>[i(w)]),_:1}),i(p,{cols:`12`},{default:l(()=>[i(x)]),_:1})]),_:1})]),_:1}))}};export{T as default};
